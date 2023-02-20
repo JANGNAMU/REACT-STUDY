4555cfd727a742fd72850cab335d068d1f9e5a5c
JANGNAMU <skagur2008@gmail.com>
2023-02-21 00:24:04 +0900

    [useRef] 사용법

--- REACT-STUDY/react-diary/src/DiaryEditor.js
+++ REACT-STUDY/react-diary/src/DiaryEditor.js
@@ -1,14 +1,79 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () =>{
-    // 작성자 state
-    const [author, setAuthor] = useState("");
+    /* 다이어리의 내용을 관리하기 위한 state */
+    const [diaryState, setDiaryState] = useState({
+        author : "",
+        content : "",
+        happy : 1
+    })
+
+    /* DOM 요소를 직접 컨트롤하기 위한 ref */
+    const authorInput = useRef()
+    const contentInput = useRef()
+
+    /* onchange 이벤트를 통해 바뀌는 값을 state에 넣어준다 */
+    const handleChangeState = e => {
+        setDiaryState({
+            ...diaryState,
+            [e.target.name] : e.target.value,
+        })
+    }
+
+    /* submit을 위한 함수 */
+    const handleSubmit = e => {
+        if(diaryState.author.length < 1){
+            // alert('작성자는 1글자 이상 입력해주세요')
+            authorInput.current.focus()
+            return;
+        }
+        if(diaryState.content.length < 1){
+            // alert('일기 내용은 5글자 이상 입력해주세요')
+            contentInput.current.focus()
+            return;
+        }
+        alert('저장되었습니다!')
+    }
 
     return (
         <div className="DiaryEditor">
             <h2>오늘의 일기</h2>
             <div>
-                <input />
+                <input
+                    ref={authorInput}
+                    name='author'
+                    value={diaryState.author} 
+                    onChange={handleChangeState}
+                    placeholder = {'작성자를 입력해주세요!'}
+                />
+            </div>
+            <div>
+                <textarea
+                    ref={contentInput}
+                    name='content'
+                    value={diaryState.content}
+                    onChange={handleChangeState}
+                    placeholder = {'내용을 입력해주세요!'}
+                />
+            </div>
+            <div>
+                <label>오늘의 행복 점수 : </label>
+                <select
+                    name='happy'
+                    value={diaryState.happy}
+                    onChange={handleChangeState}
+                >
+                    <option value={1}>🥲</option>
+                    <option value={2}>😐</option>
+                    <option value={3}>😌</option>
+                    <option value={4}>😊</option>
+                    <option value={5}>🥰</option>
+                </select>
+            </div>
+            <div>
+                <button
+                    onClick={handleSubmit}
+                >일기 저장하기</button>
             </div>
         </div>
     )
